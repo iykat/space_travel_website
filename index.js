7b319385b0a1a255159137893f88bcd091d7321c
Prince Takyi <takyip28@gmail.com>
2022-04-12 08:56:59 +0000

    add accessibility tweaks

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ navToggle.addEventListener("click", () => {
     const visibility = nav.getAttribute("data-visible");
     if (visibility === "false") {
         nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
     } else {
         nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", "false")
     }
 })
+
