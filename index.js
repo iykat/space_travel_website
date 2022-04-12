66d5f1e7a6760ba196629955b020c14738b7c95f
Prince Takyi <takyip28@gmail.com>
2022-04-12 08:09:20 +0000

    add nav functionality

---
+++ index.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+
+    const visibility = nav.getAttribute("data-visible");
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+    }
+})
